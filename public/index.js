37e9c23444e191adf84737f2ab7360d9c7409b66
baramsivaramireddy <baramsivarami@gmail.com>
2022-09-20 08:49:44 +0530

    redesign

--- public/index.js
+++ public/index.js
@@ -50,7 +50,9 @@
     const errorCode = error.code;
     const errorMessage = error.message;
         console.log(errorCode,errorMessage)
+        alert(errorMessage)
   });
   }
 
 
+export default create
